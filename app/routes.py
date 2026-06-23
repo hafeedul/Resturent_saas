c16c813a01a7c18ff1e450d74f2f2b88cf31d287
hafeedul <hafeedul@users.noreply.github.com>
2026-06-23 17:07:37 +0500

    Complete Day 1 & 2: Stitch UI Design System

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,26 @@
+from flask import render_template
 from flask import current_app as app
 
 @app.route('/')
 def home():
-    return "Hello, Portable SaaS! Running on Flask with Application Factory."
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/orders')
+def orders():
+    return render_template('order.html')
